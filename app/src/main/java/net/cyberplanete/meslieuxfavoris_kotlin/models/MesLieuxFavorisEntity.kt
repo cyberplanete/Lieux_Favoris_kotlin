233f8ddc611a8bdad73461e65fdddfe212de0827
pascal <pascal.joret@gmail.com>
2022-05-31 15:51:27 +0200

    Correction de la classe entity

--- app/src/main/java/net/cyberplanete/meslieuxfavoris_kotlin/models/MesLieuxFavorisEntity.kt
+++ app/src/main/java/net/cyberplanete/meslieuxfavoris_kotlin/models/MesLieuxFavorisEntity.kt
@@ -5,13 +5,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "MesLieuxFavorisTable")
 data class MesLieuxFavorisEntity(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val image: String,
     val description: String,
     val date: String,
-    val location: String,
+    val localisation: String,
     val latitude: Double,
     val longitude: Double
 )
