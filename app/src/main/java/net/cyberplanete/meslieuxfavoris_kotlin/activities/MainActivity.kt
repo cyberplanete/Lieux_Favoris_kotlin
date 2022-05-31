3dac647323d927b3b525ae7b010a3b3f5270b978
pascal <pascal.joret@gmail.com>
2022-05-31 15:52:16 +0200

    Déplacement de mainActivity

---
+++ app/src/main/java/net/cyberplanete/meslieuxfavoris_kotlin/activities/MainActivity.kt
@@ -0,0 +1,24 @@
+package net.cyberplanete.meslieuxfavoris_kotlin.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.cyberplanete.meslieuxfavoris_kotlin.databinding.ActivityMainBinding
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding?.root)
+
+        binding?.fabAddFavoritePlace?.setOnClickListener {
+            val intent = Intent(this, AddFavouritePlaces::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
